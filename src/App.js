6b60d28e489c6905706b3c702d3bd11722a1dc2f
Aelan Anna Mathew <aelanannamathew@gmail.com>
2023-08-17 15:52:19 +0530

    comments updated

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         This is a demo project created by Irine
+         This is a demo project created by Aelan
         </p>
         <a
           className="App-link"
